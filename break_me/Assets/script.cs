229855de1819d3aedaf8afc517250a4ae6db5013
GarinIvan <ivangarin08@gmail.com>
2024-04-24 18:16:05 +0300

    Commit
    Break

--- break_me/Assets/script.cs
+++ break_me/Assets/script.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class script : MonoBehaviour
 {
+    public float Eduard;
+    public float Ivan;
+    public float Break;
     // Start is called before the first frame update
     void Start()
     {
